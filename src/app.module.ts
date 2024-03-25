2c245bbee36b9a30cf4d0f6842d015f00b5f3418
Willian Alves Batista <willian.alves.b15@gmail.com>
2024-03-25 18:51:23 -0300

    config(nest): config eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservationModule } from './reservation/reservation.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ReservationModule,
+    MongooseModule.forRoot(
+      'mongodb://admin:adminpassword@localhost:27017/housi',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
