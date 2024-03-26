fc502f052a28daeba22739637e7477665095f507
Willian Alves Batista <willian.alves.b15@gmail.com>
2024-03-26 12:24:57 -0300

    feat: list date blocked

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 
 @Controller()
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
+  // GET /properties/123/busy-dates?start=2024-01-01&end=2024-01-30
+
+  // @Get('/properties/:id/busy-dates')
+  // async find(
+  //   @Param('id') id: string,
+  //   @Query('start') start?: string,
+  //   @Query('end') end?: string,
+  // ) {
+  //   console.log(start, 'start');
+  //   console.log(end, 'end');
+  //   console.log(id, 'id');
+  //   // return await this.reservationService.findPropertyId(id);
+  // }
 
   @Get('/properties/:id/busy-dates')
-  findOne(@Param('id') id: string) {
-    return this.reservationService.findPropertyId(id);
+  async findBlockedDates(
+    @Param('id') id: string,
+    @Query('start') start?: string,
+    @Query('end') end?: string,
+  ) {
+    const blockedDates = await this.reservationService.findBlockedDates(
+      id,
+      start,
+      end,
+    );
+    return blockedDates;
   }
 }
--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -10,7 +10,46 @@ export class ReservationService {
     private readonly reservationModel: Model<Reservation>,
   ) {}
 
-  async findPropertyId(propertyId: string) {
-    return await this.reservationModel.find({ propertyId }).exec();
+  async findBlockedDates(propertyId: string, start?: string, end?: string) {
+    // Montar o filtro para a consulta
+    const filter: any = { propertyId };
+
+    // Se start e end forem fornecidos, adicione ao filtro
+    if (start && end) {
+      filter.$or = [
+        { checkIn: { $gte: start, $lt: end } },
+        { checkOut: { $gt: start, $lte: end } },
+        { $and: [{ checkIn: { $lte: start } }, { checkOut: { $gte: end } }] },
+      ];
+    }
+
+    // Consulta no banco de dados
+    const reservations = await this.reservationModel.find(filter).exec();
+
+    // Criar um array de datas bloqueadas
+    const blockedDates: string[] = [];
+    reservations.forEach((reservation) => {
+      // Converter para o fuso horário local antes de formatar
+      const checkInDate = new Date(reservation.checkIn);
+      const checkOutDate = new Date(reservation.checkOut);
+
+      // Subtrair um dia para corrigir a diferença
+      checkInDate.setDate(checkInDate.getDate() - 1);
+      checkOutDate.setDate(checkOutDate.getDate() - 1);
+
+      // Adicionar datas ao array
+      blockedDates.push(checkInDate.toISOString().split('T')[0]);
+      blockedDates.push(checkOutDate.toISOString().split('T')[0]);
+    });
+
+    // Filtrar e retornar datas únicas dentro do intervalo
+    const uniqueBlockedDates = blockedDates
+      .filter(
+        (date, index, self) =>
+          self.indexOf(date) === index && date >= start && date <= end,
+      )
+      .sort();
+
+    return uniqueBlockedDates;
   }
 }
