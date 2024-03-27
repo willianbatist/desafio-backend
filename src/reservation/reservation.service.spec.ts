d1e02cde39513c9015687aed5bea36b46984e9f3
Willian Alves Batista <willian.alves.b15@gmail.com>
2024-03-26 23:44:28 -0300

    fix: tests service

--- src/reservation/reservation.service.spec.ts
+++ src/reservation/reservation.service.spec.ts
@@ -10,8 +10,20 @@ describe('ReservationService', () => {
       providers: [
         ReservationService,
         {
-          provide: getModelToken('reservation'), // Use getModelToken para obter o token do modelo
-          useValue: {}, // Fornecer uma instância mock vazia para o modelo
+          provide: getModelToken('reservation'),
+          useValue: {
+            find: jest.fn().mockReturnThis(),
+            exec: jest.fn().mockResolvedValue([
+              {
+                checkIn: new Date('2024-04-01'),
+                checkOut: new Date('2024-04-05'),
+              },
+              {
+                checkIn: new Date('2024-05-01'),
+                checkOut: new Date('2024-05-10'),
+              },
+            ]),
+          },
         },
       ],
     }).compile();
@@ -19,7 +31,50 @@ describe('ReservationService', () => {
     service = module.get<ReservationService>(ReservationService);
   });
 
-  it('should be defined', () => {
+  it('deve definir ReservationService', () => {
     expect(service).toBeDefined();
+  });
+
+  it('deve retornar datas bloqueadas exclusivas dentro de um intervalo específico', async () => {
+    const blockedDates = await service.findBlockedDates(
+      'propertyId',
+      '2024-04-01',
+      '2024-05-01',
+    );
+    const expectedDates = [
+      '2024-04-01',
+      '2024-04-02',
+      '2024-04-03',
+      '2024-04-04',
+      '2024-04-05',
+      '2024-05-01',
+    ];
+    expect(
+      blockedDates.every((date) => expectedDates.includes(date)),
+    ).toBeTruthy();
+  });
+
+  it('deve retornar um array vazio se nenhuma reserva for encontrada', async () => {
+    const blockedDates = await service.findBlockedDates(
+      'propertyId',
+      '2024-01-01',
+      '2024-02-01',
+    );
+    expect(blockedDates).toEqual([]);
+  });
+
+  it('Caso start e end não sejam informado, deve retornar todos os bloqueios de hoje em diante', async () => {
+    const blockedDates = await service.findBlockedDates('propertyId');
+    const expectedDates = [
+      '2024-04-01',
+      '2024-04-02',
+      '2024-04-03',
+      '2024-04-04',
+      '2024-04-05',
+      '2024-05-01',
+    ];
+    expect(
+      blockedDates.every((date) => expectedDates.includes(date)),
+    ).toBeDefined();
   });
 });
